9d2ef23469b81b97870d0e8573e11709f059a2fe
awaiss <awaiss319@protonmail.com>
2020-11-11 10:24:19 +0400

    Added assembly information.

--- AuVilator.WPF/Properties/AssemblyInfo.cs
+++ AuVilator.WPF/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AuVilator.WPF")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("AuVilator")]
+[assembly: AssemblyDescription("AuVilator for windows")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AuVilator.WPF")]
+[assembly: AssemblyCompany("zamods@github")]
+[assembly: AssemblyProduct("AuVilator")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -53,3 +53,4 @@ using System.Windows;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
